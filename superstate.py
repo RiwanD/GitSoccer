12015c1f4d1c26042b6e57cfb77534eccb848e7b
RiwanD <36884641+RiwanD@users.noreply.github.com>
2019-03-30 17:30:18 +0100

    Add files via upload
    - Soccerstate améloiré, fonction opposants, coéquipiers...

--- superstate.py
+++ superstate.py
@@ -14,15 +14,53 @@ def Superstate(object):
         self.state = state
         self.id_player = id_player
         self.id_team = id_team
-        
+
+# Balle
     @property
     def ball(self):
         return self.state.ball.position
-    
+# Joueur
+    @property
+    def team(self):
+        return self.state.player_state(self.id_team)
+
     @property
     def position(self):
         return self.state.player_state(self.id_team, self.id_player).position
+
+    @property
+    def player(self):
+		return self.state.state_player(self.id_team, self.id_player)
     
+    @property
+    def vitesse(self):
+        return self.state.player_state(self.id_team, self.id_player).vitesse
+
+	@property
+	def nearest(self):
+		return [(player, position) for 
+
+#Equpier 
+
+	@property
+	def mate(self):
+		return [self.state.state_player(id_team, id_player).position for (id_team, id_player) in self.state.players if id_team == self.id_team]
+		
+	@property
+	def near_mate(self):
+		return min([(self.player.position.distance(player), player) for player in mate])
+
+# Adversaire
+
+    @property
+    def opponents(self):
+		return [self.state.state_player(id_team, id_player).position for (id_team, id_player) in self.state.players if id_team != self.id_team]
+	
+	@property
+	def near_opp(self):
+		return min([(self.player.distance(player), player) for player in opponents])
+
+# Terrain
     @property
     def cage(self):
         if self.state.player_state(self.id_team) == 1:
@@ -37,11 +75,4 @@ def Superstate(object):
         else:
             return Vector2D(GAME_WIDTH, GAME_HEIGHT/2 - BALL_RADIUS)
     
-    @property
-    def team(self):
-        return self.state.player_state(self.id_team)
-    
-    @property
-    def vitesse(self):
-        return self.state.player_state(self.id_team, self.id_player).vitesse
-    
+        
